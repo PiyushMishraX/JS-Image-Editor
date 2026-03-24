96eb638b609ada31c1a346c53267569271587149
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-03-24 23:43:24 +0530

    editing properties

---
+++ script.js
@@ -0,0 +1,12 @@
+const filters = {
+    brightness: 100,
+    constrast: 100,
+    exposure: 100,
+    stauration: 100,
+    hueRotation: 0,
+    blut: 0,
+    grayScale: 0,
+    sepia: 0,
+    opacity: 100,
+    invert: 0,
+}
